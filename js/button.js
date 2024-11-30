09279e43beb608741987e74a51d5a33a019dcca4
Heny <1942728840@qq.com>
2024-12-01 00:30:06 +0800

    formatting

--- js/button.js
+++ js/button.js
@@ -1,10 +1,10 @@
 // 用于按钮的函数方法
-function showtip(element){
-    element.style.display="block";
+function showtip(element) {
+    element.style.display = "block";
 }
-function closetip(element){
-    element.style.display="none";
+function closetip(element) {
+    element.style.display = "none";
 }
-function jump(url){
-    window.location.href=url;
+function jump(url) {
+    window.location.href = url;
 }
